ca4afb6eb5a68c1d4ae5183b861e9ec74fe79987
Ben <70489313+BenWilliams62@users.noreply.github.com>
2022-09-04 09:47:33 +0100

    Small Change

--- frontend/pages/privacy.js
+++ frontend/pages/privacy.js
@@ -15,7 +15,7 @@ const Privacy = ({loggedIn}) => {
             <div className={styles.container}>
 
                 <h1 className={styles.head}>
-                    Privacy Policy 
+                    Privacy Policy
                 </h1>
                 <p className={styles.body}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
